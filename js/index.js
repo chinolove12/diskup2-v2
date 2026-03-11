68d603273d40270389c0538afc9240fbcf65dcf9
chinolove12 <82992425+chinolove12@users.noreply.github.com>
2026-03-11 17:46:36 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,24 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>収支管理</title>
-<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+const API_SUMMARY = "https://script.google.com/macros/s/AKfycbwjF8qz_ZOQJLbczkAuUS_xFb_OCg-W0QRSL7X8mclh_cknMX_l_J_zjU6zTl3vcWPK/exec?mode=summary"; // GAS URLに置換
 
-<h1 class="title">収支管理</h1>
-
-<div class="menu">
-
-  <div>
-    <a href="history.html" class="menu-button">履歴入力</a>
-    <div class="menu-info" id="info-history">
-      累計ゲーム数: -<br>
-      BIG合計: -<br>
-      REG合計: -<br>
-      BIG確率: -<br>
-      REG確率: -<br>
-      合計差枚: -<br>
-      ビタ成功率合計: -<br>
-      期待値合計: -<br>
-      欠損合計: -
-    </div>
-  </div>
-
-</div>
-
-<div class="bottom-menu">
-  <a href="index.html" class="bottom-item">🏠<br>Home</a>
-  <a href="history.html" class="bottom-item">📝<br>履歴</a>
-  <a href="monthly.html" class="bottom-item">📅<br>カレンダー</a>
-  <a href="data.html" class="bottom-item">📊<br>データ</a>
-</div>
-
-<script src="js/index.js"></script>
-</body>
-</html>
+// summaryを取得して履歴入力下に表示
+fetch(API_SUMMARY)
+  .then(res => res.json())
+  .then(summary => {
+    const el = document.getElementById("info-history");
+    if(el){
+      el.innerHTML = `
+        累計ゲーム数: ${summary.B3}<br>
+        BIG合計: ${summary.C3}<br>
+        REG合計: ${summary.D3}<br>
+        BIG確率: ${summary.E3}<br>
+        REG確率: ${summary.F3}<br>
+        合計差枚: ${summary.H3}<br>
+        ビタ成功率合計: ${summary.J3}<br>
+        期待値合計: ${summary.K3}<br>
+        欠損合計: ${summary.L3}
+      `;
+    }
+  })
+  .catch(err => {
+    console.error("summary取得エラー", err);
+  });
